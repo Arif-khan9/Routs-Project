217c6cf0e021f6ac9262ec07ed9b51f8c421b653
Arif-khan9 <ak4040651@gmail.com>
2025-11-23 20:18:29 +0530

    add-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
 import Home from './Pages/Home';
 import Products from './Pages/Products';
 import About from './Pages/About';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
+import JobsScond from './Pages/JobsScond';
+import SinglePage from './Pages/SinglePage';
+
+
+import Navbar from './components/Navbar';
 import ContactForm from './components/ContactForm';
 import Info from './components/Info';
 import Layout from './components/Layout/Layout';
 import NotFound from './components/NotFound';
-import JobsScond from './Pages/JobsScond';
-import SinglePage from './Pages/SinglePage';
 
 const App = () => {
   return (
-   <>
-   <Navbar/>
-   <Routes>
-    <Route path='/' element={<Home/>}/>   
-    <Route path='products' element={<Products/>}/>   
-    <Route path='/this' element={<NotFound/>}/>   
-    <Route path='about' element={<About/>}/>   
-    <Route path='jobsScond' element={<JobsScond/>}/>  
-    <Route path='jobsScond/singlePage/:id' element={<SinglePage/>}/>  
+    <>
+      <Navbar />
 
-   <Route path="contact" element={<Layout />}>
-  <Route path="contactForm" element={<ContactForm />} />
-  <Route path="info" element={<Info />} />
-  </Route>
-  <Route path="*" element={<NotFound />} />
-   </Routes>
-  
-   </>
-  )
-}
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="about" element={<About />} />
+        <Route path="jobsScond" element={<JobsScond />} />
+        <Route path="jobsScond/singlePage/:id" element={<SinglePage />} />
+
+        <Route path="contact" element={<Layout />}>
+          <Route path="contactForm" element={<ContactForm />} />
+          <Route path="info" element={<Info />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
